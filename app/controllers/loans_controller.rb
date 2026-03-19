21f930d99b1385b4bcb379dd0eddf642d74f2fd8
Katie Wood <kwood6319@hotmail.co.uk>
2026-03-20 00:38:12 +0900

    New loan form working

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,5 +1,37 @@
 class LoansController < ApplicationController
+  before_action :set_loans, only: [ :new, :create ]
+  before_action :set_students, only: [ :new, :create ]
+  before_action :set_books, only: [ :new, :create ]
+
   def new
+    @loan = Loan.new
+  end
+
+  def create
+    @loan = Loan.new(loan_params)
+
+    if @loan.save
+      redirect_to new_loan_path, notice: "Book loaned"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def loan_params
+    params.require(:loan).permit(:book_id, :student_id, :date_borrowed, :date_due)
+  end
+
+  def set_loans
     @loans = Loan.all
+  end
+
+  def set_students
+    @students = Student.all
+  end
+
+  def set_books
+    @books = Book.all
   end
 end
