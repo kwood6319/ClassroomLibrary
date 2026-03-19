7300a7125ce9e13a360e5c05cda6d7d6df7d7083
Katie Wood <kwood6319@hotmail.co.uk>
2026-03-19 23:24:12 +0900

    Validations and associations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+  has_many :books
+
+  def name
+    "#{first_name} #{second_name}"
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+  has_many :loans
+
+  def name
+    "#{first_name} #{second_name}"
+  end
 end
