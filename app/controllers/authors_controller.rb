26995e999ab3a57ec6a686a76d4c7b6d082f4139
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-01 19:36:00 +0900

    Added new and create methods to author controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,21 @@
 class AuthorsController < ApplicationController
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to books_path, notice: "Author was successfully registered!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :second_name)
+  end
 end
