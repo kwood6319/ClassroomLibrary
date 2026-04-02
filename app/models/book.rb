b67ce16cf937b5dbe9617d3a2f3354b0ac381dfe
Katie Wood <kwood6319@hotmail.co.uk>
2026-04-02 21:05:47 +0900

    Added new author method in book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,26 @@ class Book < ApplicationRecord
   belongs_to :author
   has_many :loans
 
+  attr_accessor :new_author_first_name, :new_author_second_name
+
+  before_validation :assign_author
+
   def archive!
     update!(active: false)
   end
 
   def activate!
     update!(active: true)
+  end
+
+  private
+
+  def assign_author
+    if new_author_first_name.present? || new_author_last_name.present?
+      self.author = Author.find_or_create_by(
+        first_name: new_author_first_name,
+        second_name: new_author_second_name
+      )
+    end
   end
 end
